e04914f02a7a0ae7b8be154d42eea4095b3e4d46
yzhuan-yy <cye8604@gmail.com>
2026-03-06 12:14:04 +0800

    Site updated: 2026-03-06 12:14:04

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1772769392745";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772770443959";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
